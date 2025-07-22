0a813855ebe0978ba8dfb5bdf10b9babedb504f6
Abash Sharma <105529788+abashsharma@users.noreply.github.com>
2025-07-22 17:32:23 -0500

    Create ico_generator.h
    Signed-off-by: Abash Sharma <105529788+abashsharma@users.noreply.github.com>

---
+++ Capsid/icosahedron/ico_generator.h
@@ -0,0 +1,24 @@
+#ifndef ICOSAHEDRON_H
+#define ICOSAHEDRON_H
+
+// #include <vector>
+
+struct Vec3 {
+    double x, y, z;
+
+    Vec3();
+    Vec3(double x_, double y_, double z_);
+
+    Vec3 operator+(const Vec3& v) const;
+    Vec3 operator-(const Vec3& v) const;
+    Vec3 operator*(double s) const;
+
+    bool operator<(const Vec3& other) const;
+
+    void normalize();
+};
+
+// Generates approximately `desired_points` distributed across the surface of a geometric icosahedron
+std::vector<Vec3> generate_icosahedron_surface_points(int desired_points = 400);
+
+#endif  // ICOSAHEDRON_H
