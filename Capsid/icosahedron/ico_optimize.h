85ab74ae6b1a72884e8e7a60955e6c418d6b7e9c
Abash Sharma <105529788+abashsharma@users.noreply.github.com>
2025-08-01 02:45:39 -0500

    Update ico_optimize.h
    Signed-off-by: Abash Sharma <105529788+abashsharma@users.noreply.github.com>

--- Capsid/icosahedron/ico_optimize.h
+++ Capsid/icosahedron/ico_optimize.h
@@ -15,16 +15,15 @@
 
 
 // Compute RMSD between two vectors of Vec3D points
-double compute_rmsd(const std::vector<Vec3D>& ref, const std::vector<Vec3D>& target);
+double compute_rmsd(const std::vector<Vec3>& ref, const std::vector<Vec3>& target);
 
 // Center points by subtracting centroid from all points
-std::vector<Vec3D> center_points(const std::vector<Vec3D>& points);
+std::vector<Vec3> center_points(const std::vector<Vec3>& points);
 
 // Optimize the harmonics shape to match ico vertices via fixed nearest-point assignment
 // h: harmonic shape to optimize
 // ico_vertices: fixed icosahedron vertex points
 // max_iterations: number of optimization iterations (default 100)
-void ico_optimize(capsid::Harmonics& h,
-                  const std::vector<Vec3D>& ico_vertices);
+void ico_optimize(capsid::Harmonics& h);
 
 #endif // ICO_OPTIMIZE_HPP
